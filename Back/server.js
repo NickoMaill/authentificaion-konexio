f248e153c8e30ff7b997d4389303460a82f2bf5d
Nicolas-Maillols <nicomaillols@gmail.com>
2022-03-28 14:55:12 +0200

    set and clean react app all files and connecting to DB

---
+++ Back/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRouter = require("./routes/userRoute");
+dotenv.config({
+  path: "./config.env",
+});
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("connected to DB"));
+
+app.use(express.json())
+app.use("/user", userRouter)
+
+app.listen(8000, () => console.log("listening on port 8000"));
